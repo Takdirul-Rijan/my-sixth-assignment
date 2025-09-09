c2a02b737f3d39b56d5fc79542b90510f311c58c
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-09-09 09:49:24 +0600

    added all functionalities

--- README.md
+++ README.md
@@ -1,11 +1,55 @@
-#### 7) Create a README file to answer the following question-
+1. What is the difference between var, let, and const?
 
-#### 1) What is the difference between var, let, and const?
+Answer: var: Old way of declaring variables. Function-scoped, can be re-declared and updated.
 
-#### 2) What is the difference between map(), forEach(), and filter()?
+let: Introduced in ES6. It is block-scoped (only available inside { }). It can be updated but cannot be re-declared in the same scope. Safer than var.
 
-#### 3) What are arrow functions in ES6?
+const: Also block-scoped, cannot be re-assigned (but objects/arrays can still be modified).
 
-#### 4) How does destructuring assignment work in ES6?
+2. What is the difference between map(), forEach(), and filter()?
 
-#### 5) Explain template literals in ES6. How are they different from string concatenation?
+Answer: map(): Creates and returns a new array by applying a function to each element. Length of the new array = original array. Example: [4,8,12].map(x=>x/2) ----> [2,4,6]
+
+forEach(): Loops through each element and executes a function, but does not return anything (always undefined). Example: [1,2,3].forEach(x=>console.log(x)).
+
+filter(): Creates and returns a new array containing only elements that pass a condition. Example: [1,2,3,4].filter(x=>x%2===0) → [2,4].
+
+3. What are arrow functions in ES6?
+
+Answer: A shorter and cleaner syntax for writing functions.
+Example: Normal function: function add(a, b) { return a + b; }
+
+Arrow function: const add = (a, b) => a + b;
+
+4. How does destructuring assignment work in ES6?
+
+Answer: A way to unpack values from arrays or properties from objects into separate variables.
+
+Array: const [first, second] = [10, 20]; ---> first=10, second=20
+
+Object: const {name, age} = {name:"Ron", age:22}; ---> name="Ron", age=22
+
+This avoids writing repetitive code and makes extraction cleaner.
+
+5. Explain template literals in ES6. How are they different from string concatenation?
+
+Answer: Use backticks (``) and allow ${} for variables.
+
+Example: let name = "Eric";
+console.log(`Hello, ${name}!`);
+
+Difference from concatenation: Easier syntax, supports multi-line strings, and interpolation without +.
+
+Using Concatenation:
+const name = "Mark";
+const age = 45;
+const message = "My name is " + name + " and I am " + age + " years old.";
+console.log(message);
+output: My name is Mark and I am 45 years old.
+
+Using Template Literals:
+const name = "Mark";
+const age = 45;
+const message = `My name is ${name} and I am ${age} years old.`;
+console.log(message);
+output: My name is Mark and I am 45 years old
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const loadAllPlants = () => {
   fetch("https://openapi.programming-hero.com/api/plants")
     .then((res) => res.json())
-    .then((data) => displayPlants(data.plants));
+    .then((data) => {
+      displayPlants(data.plants);
+      displayCategories(data.plants);
+    });
 };
 
 const displayPlants = (plants) => {
@@ -9,14 +12,19 @@ const displayPlants = (plants) => {
   container.innerHTML = "";
 
   plants.forEach((plant) => {
+    // console.log(plant);
     const card = document.createElement("div");
     card.className =
       "bg-white rounded-xl shadow p-4 flex flex-col justify-between";
 
     card.innerHTML = `
       <img src="${plant.image}" alt="" class="w-full h-40 object-cover rounded-lg mb-3" />
-      <h3 class="font-bold text-lg mb-1 text-green-800">${plant.name}</h3>
-      <p class="text-sm text-gray-600 mb-2">${plant.description}</p>
+      <h3 class="font-bold text-lg mb-1 text-green-800 cursor-pointer hover:underline">
+        ${plant.name}
+      </h3>
+      <p class="text-sm text-gray-600 mb-2">
+        ${plant.description}
+      </p>
       <div class="flex justify-between items-center mb-2">
         <span class="badge bg-[#DCFCE7] text-green-800 rounded-full px-3 py-1 text-sm">
           ${plant.category}
@@ -28,8 +36,146 @@ const displayPlants = (plants) => {
       </button>
     `;
 
+    card.querySelector("h3").addEventListener("click", () => {
+      // console.log("btn cliked");
+
+      openModal(plant);
+    });
+
+    card.querySelector("button").addEventListener("click", () => {
+      // console.log("cart btnclicked");
+
+      const confirmPurchase = alert(
+        `${plant.name} will be added to your cart!\nPrice: ৳${plant.price}`
+      );
+      addCart(plant);
+    });
+
     container.append(card);
   });
 };
+
+const displayCategories = (plants) => {
+  const container = document.getElementById("categories-container");
+  container.innerHTML = "";
+
+  const allBtn = document.createElement("button");
+  allBtn.innerText = "All Trees";
+  allBtn.className =
+    "block w-full text-center md:text-left px-4 py-3 rounded-md text-semibold text-grey-900";
+  allBtn.onclick = () => {
+    activeBtn(allBtn);
+    manageSpinner(plants);
+  };
+  container.appendChild(allBtn);
+
+  const addedCategories = [];
+
+  plants.forEach((plant) => {
+    if (!addedCategories.includes(plant.category)) {
+      addedCategories.push(plant.category);
+
+      const btn = document.createElement("button");
+      btn.innerText = plant.category;
+      btn.className =
+        "block w-full text-center md:text-left px-4 py-3 rounded-md text-gray-900";
+
+      btn.onclick = () => {
+        const filtered = plants.filter((p) => p.category === plant.category);
+        manageSpinner(filtered);
+        activeBtn(btn);
+      };
+
+      container.append(btn);
+    }
+  });
+};
+
+let cart = [];
+
+function addCart(plant) {
+  cart.push(plant);
+  showCart();
+}
+
+function showCart() {
+  const container = document.getElementById("cart-container");
+  container.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item, index) => {
+    // console.log("[0]");
+
+    total += item.price;
+
+    const row = document.createElement("div");
+    row.className =
+      "flex justify-between items-center bg-[#F0FDF4] rounded-2xl m-2 p-4";
+
+    row.innerHTML = `
+    <div class="grid grid-cols-1"><span class="my-2">${item.name}</span>
+      <span class="mb-2">৳${item.price}</span ></div>
+      
+      <button class="text-red-500">❌</button>
+    `;
+
+    row.querySelector("button").onclick = () => removeFromCart(index);
+
+    container.appendChild(row);
+  });
+
+  document.getElementById("cart-total").innerText = "৳" + total;
+}
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  showCart();
+}
+// console.log(cart);
+
+const openModal = (plant) => {
+  const modal = document.getElementById("plant-modal");
+  modal.innerHTML = `
+    <div class="fixed inset-0 z-50 flex items-center justify-center">
+      <div class="absolute inset-0 bg-black opacity-40" onclick="closeModal()"></div>
+      <div class="bg-white rounded-xl shadow-lg z-10 max-w-xl w-full p-6">
+        <div class="flex justify-between items-start">
+          <h3 class="text-2xl font-bold text-green-800">${plant.name}</h3>
+          <button class="text-gray-500" onclick="closeModal()">✖</button>
+        </div>
+        <img src="${plant.image}" alt="" class="w-full h-56 object-cover rounded-lg my-4" />
+        <p class="text-gray-700 mb-3">${plant.description}</p>
+        <div class="flex justify-between items-center">
+          <span class="badge bg-[#DCFCE7] text-green-800 px-3 py-1">${plant.category}</span>
+          <span class="font-semibold">৳${plant.price}</span>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+const closeModal = () => {
+  document.getElementById("plant-modal").innerHTML = "";
+};
+
+const activeBtn = (btn) => {
+  const allBtns = document
+    .getElementById("categories-container")
+    .querySelectorAll("button");
+  allBtns.forEach((b) => b.classList.remove("bg-green-700", "text-white"));
+  btn.classList.add("bg-green-700", "text-white");
+};
+
+function manageSpinner(list) {
+  showSpinner(true);
+  setTimeout(() => {
+    displayPlants(list);
+    showSpinner(false);
+  }, 100);
+}
+
+function showSpinner(isShow) {
+  document.getElementById("spinner").style.display = isShow ? "flex" : "none";
+}
 
 loadAllPlants();
